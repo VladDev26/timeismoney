71c5a4518b11bb2ac77c2718d0745ac4b96daebe
VladDev26 <volodimirb23@gmail.com>
2017-02-15 00:18:13 +0200

    webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,63 @@
-let path = require('path');
-let webpack = require('webpack');
-let ExtractTextPlugin = require('extract-text-webpack-plugin'); //separate css from js
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
-let production = new webpack.DefinePlugin({
-  'process.env': { NODE_ENV: JSON.stringify('production') }
+var production = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
 });
 
 
 module.exports = {
-  entry: path.resolve(__dirname + '/src/entry.js'),
+  entry: path.resolve(__dirname + '/src/index.js'),
+
   output: {
-    path: 'assets',
-    filename: 'js/bundle.min.js',
+    path: path.join(__dirname, "assets"),
+    filename: 'js/bundle.js',
     publicPath: '/assets/'
   },
   
   module: {
-    loaders: [
+    rules: [
+      // { 
+      //   test: /\.js$/, 
+      //   exclude: /(node_modules)/,
+      //   loader: 'file-loader?name=js/[name].[ext]'
+      // },
+
       { 
-        test: /\.scss$/i, 
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+        test: /\.js$/, 
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { presets: ['es2015'] }
       },
       
+      { 
+        test: /\.scss$/i, 
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
 
-      // FONTS
       { 
         test: /.*\.(ttf|eot|woff2?|svg)(\?.*$|$)/i,
-        loader: "file?name=fonts/[name]/[name].[ext]"
+        loader: "file-loader?name=fonts/[name]/[name].[ext]"
       },
 
-      // HTML
       { 
         test: /\.html$/i,
-        loader: "html"
+        loader: "html-loader"
       },
 
-
-       // IMG
       { 
         test: /.*\.(gif|png|jpg)$/i, 
-        loader: "file?name=img/[name].[ext]"
+        loader: "file-loader?name=img/[name].[ext]"
       }
     ],
   },
   plugins: [
-    new ExtractTextPlugin('styles/bundle.min.css'),
-    new webpack.optimize.UglifyJsPlugin()
+    // production,
+    // new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('styles/bundle.min.css')
   ]
 };
 
@@ -54,3 +65,78 @@ module.exports = {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let path = require('path');
+// let webpack = require('webpack');
+// let ExtractTextPlugin = require('extract-text-webpack-plugin'); //separate css from js
+
+
+// let production = new webpack.DefinePlugin({
+//   'process.env': { NODE_ENV: JSON.stringify('production') }
+// });
+
+
+// module.exports = {
+//   entry: path.resolve(__dirname + '/src/entry.js'),
+//   output: {
+//     path: 'assets',
+//     filename: 'js/bundle.min.js',
+//     publicPath: '/assets/'
+//   },
+  
+//   module: {
+//     loaders: [
+//       { 
+//         test: /\.scss$/i, 
+//         loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+//       },
+      
+
+//       { 
+//         test: /.*\.(ttf|eot|woff2?|svg)(\?.*$|$)/i,
+//         loader: "file?name=fonts/[name]/[name].[ext]"
+//       },
+
+//       { 
+//         test: /\.html$/i,
+//         loader: "html"
+//       },
+
+
+//       { 
+//         test: /.*\.(gif|png|jpg)$/i, 
+//         loader: "file?name=img/[name].[ext]"
+//       }
+//     ],
+//   },
+//   plugins: [
+//     new ExtractTextPlugin('styles/bundle.min.css'),
+//     new webpack.optimize.UglifyJsPlugin()
+//   ]
+// };
